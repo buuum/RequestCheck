7efd8dd3cd0933b0d261f032da60370c44ad57ef
Ferran Martinez <alfonsmartinezparra@gmail.com>
2017-12-12 16:45:50 +0100

    add getErrorsKeys

--- src/RequestCheck/RequestResponse.php
+++ src/RequestCheck/RequestResponse.php
@@ -7,6 +7,7 @@ class RequestResponse
     private $errors = [];
     private $messages = [];
     private $data;
+    private $errorsKeys;
 
     public function __construct($messages = [])
     {
@@ -15,7 +16,26 @@ class RequestResponse
 
     public function addError($fieldError)
     {
+        $this->errorsKeys[$fieldError->name()] = $this->parseErrorsKeys($fieldError);
         $this->errors[] = $fieldError;
+    }
+
+    private function parseErrorsKeys($fieldError)
+    {
+        if (!empty($fieldError->subfields())) {
+            return $this->errorsKeys($fieldError->subfields());
+        }
+        return [];
+    }
+
+    private function errorsKeys($subfields)
+    {
+        $parts = [];
+        foreach ($subfields as $fieldError) {
+            $parts[][$fieldError->name()] = $this->parseErrorsKeys($fieldError);
+        }
+
+        return $parts;
     }
 
     public function setData($data)
@@ -36,6 +56,11 @@ class RequestResponse
     public function getErrors(): array
     {
         return $this->errors;
+    }
+
+    public function getErrorsKeys(): array
+    {
+        return $this->errorsKeys;
     }
 
 }
